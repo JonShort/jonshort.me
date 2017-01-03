9595603d7562283c490ebbb31bf963f04962f366
Jon Short <JonShort@users.noreply.github.com>
2017-01-03 18:32:04 +0000

    Fixes/fix merge conflict (#13)
    * Added react
    
    * Added Tachyons & standard homepage items. (#3)
    
    * Allowed article list item to access props.children (#5)
    
    * Article list now populated by static data array (#6)
    
    * Added routing and made article shell (#7)
    
    * Compressed images manually (#8)
    
    * Change names (will break app) (#10)
    
    * Feature/merge in develop (#11)
    
    * Merging in develop (#4)
    
    * Added react
    
    * Added Tachyons & standard homepage items. (#3)
    
    * merge in the compressed images

---
+++ src/data/articleData.jsx
@@ -0,0 +1,14 @@
+import ArticleImage from '../images/articles/react-logo.png';
+
+var articleData = [
+    {
+        imageSrc: ArticleImage,
+        imageAlt: 'Image of the React logo',
+        url: '/why-react-tachyons',
+        date: '2 January, 2017',
+        title: 'hello',
+        subtitle: 'Some information here'
+    },
+];
+
+export default articleData;
