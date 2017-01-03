d4ca158862439aa6b1850bb33f873dc3bc8a7e16
Jon Short <JonShort@users.noreply.github.com>
2017-01-03 18:43:50 +0000

    Merge in Develop (#17)
    * Added react
    
    * Added Tachyons & standard homepage items. (#3)
    
    * Allowed article list item to access props.children (#5)
    
    * Article list now populated by static data array (#6)
    
    * Added routing and made article shell (#7)
    
    * Compressed images manually (#8)
    
    * Change names (will break app) (#10)
    
    * Feature/merge in develop (#11)
    
    * Merging in develop (#4)
    
    * Added react
    
    * Added Tachyons & standard homepage items. (#3)
    
    * merge in the compressed images
    
    * Changed names back to original, with caps (#16)

---
+++ src/homepage/ArticleList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ArticleListItem from './ArticleListItem';
+
+const ArticleList = ({articles}) => {
+
+    var articleItem = [];
+    articles.forEach(function(article){
+        articleItem.push(<ArticleListItem article={article} key={article.url} />);
+    });
+
+    return (
+        <section className="mw7 center avenir">
+            <h2 className="baskerville fw1 ph3 ph0-l">Blog</h2>
+            {articleItem}
+        </section>
+    );
+}
+
+export default ArticleList;
