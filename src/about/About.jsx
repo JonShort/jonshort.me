365f65aacf8c43f88e18d20eae3b7bce630f3cb2
Jon Short <JonShort@users.noreply.github.com>
2017-04-11 21:23:02 +0100

    updated the about section

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -1,34 +1,47 @@
 import React from 'react';
-
+import {Link} from 'react-router';
 const About = () => {
     return (
-        <article className="cf ph3 ph5-ns pv5">
+        <section>
+        <article className="cf ph3 ph5-ns pt5 pb3">
             <header className="fn fl-ns w-50-ns pr4-ns">
-                <h1 className="mb3 mt0 lh-title">About</h1>
-                <time className="f6 ttu tracked gray">Jan Tschichold</time>
+                <h1 className="f2 lh-title fw9 mb3 mt0 pt3 bt bw2">
+                About
+                </h1>
+                <div className="pv2">
+                    <a className="link near-black hover-silver dib h2 w2 mr3" href="https://github.com/JonShort" title="GitHub" target="_blank">
+                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414"><path d="M8 0C3.58 0 0 3.582 0 8c0 3.535 2.292 6.533 5.47 7.59.4.075.547-.172.547-.385 0-.19-.007-.693-.01-1.36-2.226.483-2.695-1.073-2.695-1.073-.364-.924-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.223 1.873.87 2.33.665.072-.517.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.953 0-.873.31-1.587.823-2.147-.083-.202-.358-1.015.077-2.117 0 0 .672-.215 2.2.82.638-.178 1.323-.266 2.003-.27.68.004 1.364.092 2.003.27 1.527-1.035 2.198-.82 2.198-.82.437 1.102.163 1.915.08 2.117.513.56.823 1.274.823 2.147 0 3.073-1.87 3.75-3.653 3.947.287.246.543.735.543 1.48 0 1.07-.01 1.933-.01 2.195 0 .215.144.463.55.385C13.71 14.53 16 11.534 16 8c0-4.418-3.582-8-8-8"/></svg>
+                    </a>
+                    <a className="link hover-silver near-black dib h2 w2 mr3" href="https://twitter.com/altshort" title="Twitter" target="_blank">
+                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414"><path d="M16 3.038c-.59.26-1.22.437-1.885.517.677-.407 1.198-1.05 1.443-1.816-.634.375-1.337.648-2.085.795-.598-.638-1.45-1.036-2.396-1.036-1.812 0-3.282 1.468-3.282 3.28 0 .258.03.51.085.75C5.152 5.39 2.733 4.084 1.114 2.1.83 2.583.67 3.147.67 3.75c0 1.14.58 2.143 1.46 2.732-.538-.017-1.045-.165-1.487-.41v.04c0 1.59 1.13 2.918 2.633 3.22-.276.074-.566.114-.865.114-.21 0-.416-.02-.617-.058.418 1.304 1.63 2.253 3.067 2.28-1.124.88-2.54 1.404-4.077 1.404-.265 0-.526-.015-.783-.045 1.453.93 3.178 1.474 5.032 1.474 6.038 0 9.34-5 9.34-9.338 0-.143-.004-.284-.01-.425.64-.463 1.198-1.04 1.638-1.7z" fillRule="nonzero"/></svg>
+                    </a>
+                    <a className="link hover-silver near-black dib h2 w2 mr3" href="https://instagram.com/altshort" title="Instagram" target="_blank">
+                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1.414"><path d="M8 0C5.827 0 5.555.01 4.702.048 3.85.088 3.27.222 2.76.42c-.526.204-.973.478-1.417.923-.445.444-.72.89-.923 1.417-.198.51-.333 1.09-.372 1.942C.008 5.555 0 5.827 0 8s.01 2.445.048 3.298c.04.852.174 1.433.372 1.942.204.526.478.973.923 1.417.444.445.89.72 1.417.923.51.198 1.09.333 1.942.372.853.04 1.125.048 3.298.048s2.445-.01 3.298-.048c.852-.04 1.433-.174 1.942-.372.526-.204.973-.478 1.417-.923.445-.444.72-.89.923-1.417.198-.51.333-1.09.372-1.942.04-.853.048-1.125.048-3.298s-.01-2.445-.048-3.298c-.04-.852-.174-1.433-.372-1.942-.204-.526-.478-.973-.923-1.417-.444-.445-.89-.72-1.417-.923-.51-.198-1.09-.333-1.942-.372C10.445.008 10.173 0 8 0zm0 1.44c2.136 0 2.39.01 3.233.048.78.036 1.203.166 1.485.276.374.145.64.318.92.598.28.28.453.546.598.92.11.282.24.705.276 1.485.038.844.047 1.097.047 3.233s-.01 2.39-.048 3.233c-.036.78-.166 1.203-.276 1.485-.145.374-.318.64-.598.92-.28.28-.546.453-.92.598-.282.11-.705.24-1.485.276-.844.038-1.097.047-3.233.047s-2.39-.01-3.233-.048c-.78-.036-1.203-.166-1.485-.276-.374-.145-.64-.318-.92-.598-.28-.28-.453-.546-.598-.92-.11-.282-.24-.705-.276-1.485C1.45 10.39 1.44 10.136 1.44 8s.01-2.39.048-3.233c.036-.78.166-1.203.276-1.485.145-.374.318-.64.598-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276C5.61 1.45 5.864 1.44 8 1.44zm0 2.452c-2.27 0-4.108 1.84-4.108 4.108 0 2.27 1.84 4.108 4.108 4.108 2.27 0 4.108-1.84 4.108-4.108 0-2.27-1.84-4.108-4.108-4.108zm0 6.775c-1.473 0-2.667-1.194-2.667-2.667 0-1.473 1.194-2.667 2.667-2.667 1.473 0 2.667 1.194 2.667 2.667 0 1.473-1.194 2.667-2.667 2.667zm5.23-6.937c0 .53-.43.96-.96.96s-.96-.43-.96-.96.43-.96.96-.96.96.43.96.96z"/></svg>
+                    </a>
+                </div>
             </header>
             <div className="fn fl-ns w-50-ns">
-                <p className="lh-copy measure mt4 mt0-ns">
-                PERFECT typography is more a science than an art. Mastery of the trade is
-                indispensable, but it isn't everything. Unerring taste, the hallmark of
-                perfection, rests also upon a clear understanding of the laws of harmonious
-                design. As a rule, impeccable taste springs partly from inborn sensitivity:
-                from feeling. But feelings remain rather unproductive unless they can inspire a
-                secure judgment. Feelings have to mature into knowledge about the consequences
-                of formal decisions. For this reason, there are no born masters of typography,
-                but self- education may lead in time to mastery.
+                <p className="f5 lh-copy measure mt0-ns">
+                I am a designer / front-end developer at <a className="f5 fw6 green no-underline underline-hover" href="https://rightindem.com/" target="_blank">RightIndem</a>, 
+                focusing on best practice, maintainability, and bridging the gap between
+                design and development.
                 </p>
-                <p className="lh-copy measure">
-                It is wrong to say that there is no arguing about taste when it is good taste
-                that is in question. We are not born with good taste, nor do we come into this
-                world equipped with a real understanding of art. Merely to recognize who or
-                what is represented in a picture has little to do with a real under- standing
-                of art. Neither has an uninformed opinion about the proportions of Roman
-                letters. In any case, arguing is senseless. He who wants to convince has to
-                do a better job than others.
+                <p className="f5 lh-copy measure">
+                Recently I've been working on RightIndem's <a className="f5 fw6 blue no-underline underline-hover" href="https://facebook.github.io/react/" target="_blank">React.js</a> application,
+                to create a re-usable UI component library.
                 </p>
             </div>
         </article>
+        <div className="bb b--black-10 ph3 ph5-ns pt1 pb4">
+            <Link to="/" className="f5 no-underline black bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4">
+                <svg className="w1" data-icon="chevronLeft" viewBox="0 0 32 32" style={{fill: 'currentColor'}}>
+                    <title>chevronLeft icon</title>
+                    <path d="M20 1 L24 5 L14 16 L24 27 L20 31 L6 16 z"></path>
+                </svg>
+                <span className="pl1">Return to Home</span>
+            </Link>
+        </div>
+        </section>
     )
 };
 
