019d84ef542cb89e9c2824edd1748e261e56d750
JonShort <JonShort@users.noreply.github.com>
2020-02-12 11:03:17 +0000

    Add ability to display images within articles

--- src/articles/components/index.js
+++ src/articles/components/index.js
@@ -3,6 +3,7 @@ export { default as Code } from './Code';
 export { default as H3 } from './H3';
 export { default as Hero } from './Hero';
 export { default as HomeLink } from './HomeLink';
+export { default as Image } from './Image';
 export { default as Li } from './Li';
 export { default as P } from './P';
 export { default as Pre } from './Pre';
