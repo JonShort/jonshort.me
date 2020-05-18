c1bb09c9dffbe4d06ebcbe2853ffdfef4fadfd48
JonShort <JonShort@users.noreply.github.com>
2020-05-18 22:26:26 +0100

    Add prismjs as dep - change theme

--- src/articles/components/Code.jsx
+++ src/articles/components/Code.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
+import Prism from 'prism-react-renderer/prism';
+import dracula from 'prism-react-renderer/themes/dracula';
+
+(typeof global !== 'undefined' ? global : window).Prism = Prism;
+
+require('prismjs/components/prism-rust');
 
 const Code = ({ children, className }) => {
   const language = className.replace(/language-/, '');
 
   return (
-    <Highlight {...defaultProps} code={children} language={language}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={`overflow-x-auto ${className}`}
-          style={{ ...style, padding: '20px' }}
-        >
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
-      )}
+    <Highlight
+      {...defaultProps}
+      code={children}
+      language={language}
+      theme={dracula}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => {
+        // not sure why an extra token was being added
+        // removed here
+        tokens.pop();
+
+        return (
+          <pre
+            className={`overflow-x-auto mw7 ${className}`}
+            style={{ ...style, padding: '20px' }}
+          >
+            {tokens.map((line, i) => (
+              <div key={i} {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        );
+      }}
     </Highlight>
   );
 };
