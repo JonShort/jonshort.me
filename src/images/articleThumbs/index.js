f7eabcb9f92838d6e37c3a3320aeb9eb9136c40e
JonShort <JonShort@users.noreply.github.com>
2020-05-18 22:31:15 +0100

    Add new article thumbnail

--- src/images/articleThumbs/index.js
+++ src/images/articleThumbs/index.js
@@ -1,2 +1,3 @@
+export { default as CodeBlock } from './code-block.png';
 export { default as ReactLogo } from './react-logo.png';
-export { default as StyledComponents } from "./styled-components.png";
+export { default as StyledComponents } from './styled-components.png';
