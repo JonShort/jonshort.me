300d9d65fc51f362b1aca1f1e542fa79c62b9c99
JonShort <JonShort@users.noreply.github.com>
2020-05-18 22:32:13 +0100

    Add WIP new article

--- src/articles/articleMetaData.js
+++ src/articles/articleMetaData.js
@@ -1,9 +1,13 @@
 import { metadata as refactoringThisSite } from './posts/refactoring-this-site/metadata';
+import { metadata as smartCodePerformance } from './posts/smart-code-performance/metadata';
 
 const articleMetaData = {
-    ...refactoringThisSite,
+  ...smartCodePerformance,
+  ...refactoringThisSite,
 };
 
-export const allArticlesMetaData = Object.keys(articleMetaData).map(key => articleMetaData[key]);
+export const allArticlesMetaData = Object.keys(articleMetaData).map(
+  (key) => articleMetaData[key]
+);
 
 export default articleMetaData;
