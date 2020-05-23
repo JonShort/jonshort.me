03966e41194e9bbd0ed891f5a9543325d93422e3
JonShort <JonShort@users.noreply.github.com>
2020-05-23 16:47:46 +0100

    Update tests to use baseUrl, and arrow functions

--- cypress/integration/about.spec.js
+++ cypress/integration/about.spec.js
@@ -1,33 +1,38 @@
-describe('About page test', function() {
-  it('Visits localhost - about page', function() {
-    cy.visit('http://localhost:3000/about')
-  })
+describe('About page test', () => {
+  it('Visits about page', () => {
+    cy.visit('/about');
+  });
 
-  it('Checks the header & external links', function() {
-    cy.get('header').find('h1').should('contain', 'About')
+  it('Checks the header & external links', () => {
+    cy.get('header').find('h1').should('contain', 'About');
 
-    cy.get('header').find('a[title="GitHub"]').should('have.attr', 'href', 'https://github.com/JonShort')
+    cy.get('header')
+      .find('a[title="GitHub"]')
+      .should('have.attr', 'href', 'https://github.com/JonShort');
 
-    cy.get('header').find('a[title="Twitter"]').should('have.attr', 'href', 'https://twitter.com/altshort')
-  })
+    cy.get('header')
+      .find('a[title="Twitter"]')
+      .should('have.attr', 'href', 'https://twitter.com/altshort');
+  });
 
-  it('Checks body copy exists', function() {
-    cy
-      .get('article')
+  it('Checks body copy exists', () => {
+    cy.get('article')
       .children('div')
       .children()
-      .should(a => expect(a.length, 'body copy [array]').to.be.greaterThan(0))
-  })
+      .should((a) =>
+        expect(a.length, 'body copy [array]').to.be.greaterThan(0)
+      );
+  });
 
-  it('Navigates to homepage', function() {
-    cy.get('a:contains("Home")').click()
+  it('Navigates to homepage', () => {
+    cy.get('a:contains("Home")').click();
 
-    cy.url().should('be', 'http://localhost:3000/')
-  })
+    cy.url().should('be', '/');
+  });
 
-  it('Returns to about page', function() {
-    cy.contains('About').click()
-    
-    cy.url().should('include', '/about')
-  })
-})
+  it('Returns to about page', () => {
+    cy.contains('About').click();
+
+    cy.url().should('include', '/about');
+  });
+});
