24cd864431aea6c9afeebef45d3449c069e6e884
JonShort <JonShort@users.noreply.github.com>
2021-03-21 14:11:15 +0000

    Add article to main list

--- src/articles/articleMetaData.js
+++ src/articles/articleMetaData.js
@@ -1,8 +1,10 @@
 import { metadata as handlingLineEndings } from './posts/handling-line-endings/metadata';
+import { metadata as manipulatingProblems } from './posts/manipulating-problems/metadata';
 import { metadata as refactoringThisSite } from './posts/refactoring-this-site/metadata';
 import { metadata as smartCodePerformance } from './posts/smart-code-performance/metadata';
 
 const articleMetaData = {
+  ...manipulatingProblems,
   ...handlingLineEndings,
   ...smartCodePerformance,
   ...refactoringThisSite,
