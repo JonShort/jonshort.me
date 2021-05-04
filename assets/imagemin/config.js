9e043db4f3d2e92db4ae4a575a40b642ec0525b3
JonShort <JonShort@users.noreply.github.com>
2021-05-04 21:05:49 +0100

    ESM syntax on imagemin config

--- assets/imagemin/config.js
+++ assets/imagemin/config.js
@@ -1,29 +1,31 @@
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant');
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
 
 (async () => {
-	const rootPromise = imagemin(['assets/images/*.{jpg,png}'], {
-		destination: 'src/images',
-		plugins: [
-			imageminJpegtran(),
-			imageminPngquant({
-				quality: [0.6, 0.8],
-			})
-		]
-	});
-	
-	const thumbPromise = imagemin(['assets/images/articleThumbs/*.{jpg,png}'], {
-		destination: 'src/images/articleThumbs',
-		plugins: [
-			imageminJpegtran(),
-			imageminPngquant({
-				quality: [0.6, 0.8],
-			})
-		]
-	});
+  const rootPromise = imagemin(['assets/images/*.{jpg,png}'], {
+    destination: 'src/images',
+    plugins: [
+      imageminJpegtran(),
+      imageminPngquant({
+        quality: [0.6, 0.8],
+      }),
+    ],
+  });
 
-	const [rootFiles, thumbFiles] = await Promise.all([rootPromise, thumbPromise]);
-	console.log(rootFiles, thumbFiles);
+  const thumbPromise = imagemin(['assets/images/articleThumbs/*.{jpg,png}'], {
+    destination: 'src/images/articleThumbs',
+    plugins: [
+      imageminJpegtran(),
+      imageminPngquant({
+        quality: [0.6, 0.8],
+      }),
+    ],
+  });
+
+  const [rootFiles, thumbFiles] = await Promise.all([
+    rootPromise,
+    thumbPromise,
+  ]);
+  console.log(rootFiles, thumbFiles);
 })();
-
